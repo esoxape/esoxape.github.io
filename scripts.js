63255620bf0b5a706a549c1845efa53240deb9a4
Henrik Johansson <henrik@soltek.se>
2023-04-03 11:59:19 +0200

    update

--- scripts.js
+++ scripts.js
@@ -4,4 +4,19 @@ const playerSpeed = 5;
 
 function movePlayer(event) {
     const playerRect = player.getBoundingClientRect();
-    const gameBoardRect
+    const gameBoardRect = gameBoard.getBoundingClientRect();
+
+    if (event.key === "ArrowLeft") {
+        const newPosition = playerRect.left - playerSpeed;
+        if (newPosition >= gameBoardRect.left) {
+            player.style.left = `${newPosition}px`;
+        }
+    } else if (event.key === "ArrowRight") {
+        const newPosition = playerRect.left + playerSpeed;
+        if (newPosition + playerRect.width <= gameBoardRect.right) {
+            player.style.left = `${newPosition}px`;
+        }
+    }
+}
+
+document.addEventListener("keydown", movePlayer);
